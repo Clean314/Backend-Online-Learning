db7830ab76b86997f1367db6cb412b43278828b6
clean314 <pipie314@protonmail.com>
2025-05-15 17:32:48 +0900

    .

--- src/main/java/com/docker/backend/controller/MemberController.java
+++ src/main/java/com/docker/backend/controller/MemberController.java
@@ -1,36 +1,14 @@
 package com.docker.backend.controller;
 
-<<<<<<< Updated upstream:src/main/java/com/docker/backend/member/MemberController.java
 import com.docker.backend.model.Member;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-=======
-import com.docker.backend.dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
->>>>>>> Stashed changes:src/main/java/com/docker/backend/controller/MemberController.java
 
 @RestController
-@RequestMapping
 public class MemberController {
 
-    @PostMapping("/register")
-    public void memberRegister(Member member) {
 
-    }
-
-    @PostMapping("/login")
-    public void memberLogin(Member member) {
-    }
-
-    @GetMapping("/logout")
-    public void memberLogout() {
     }
 }
---
+++ src/main/java/com/docker/backend/model/Member.java
@@ -0,0 +1,25 @@
+package com.docker.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member")
+@Data
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role")
+    private MemberRole role;
+
+    private String email;
+    private String name;
+    private String password;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
