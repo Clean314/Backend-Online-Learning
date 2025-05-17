b2df3fc0f14dccf2fad494da0ceb6571cc0ca614
clean314 <pipie314@protonmail.com>
2025-05-17 13:26:35 +0900

    비밀번호 관련 확인

--- src/main/resources/sql/import.sql
+++ src/main/resources/sql/import.sql
@@ -1,8 +1,12 @@
 -- member
+-- DB 에만 보안 때문에 비번 이렇게 저장하는 거고 백엔드에서는 암호화 상태를 간주하고 파싱하기 때문에 이걸 그대로 넣어서 테스트하면 안됨
+-- 암호화 쉽게 만드는 사이트 : https://bcrypt-generator.com/
+-- [비번 목록]
+-- 1: test111, 2: test222, 3: test333, 4: test444
 INSERT INTO member (email, password, name, role, created_at) VALUES ('test@email.com', '{bcrypt}$2a$12$YKw2mg1Uu7zr/Diw3NwVUe6UozIO179NKNbB0TH012sa4IHjAfYVi', '홍길동', 'STUDENT', now());
-INSERT INTO member (email, password, name, role, created_at) VALUES ('test11@email.com', '{bcrypt}$2a$12$8SBxYG988lsn6SBS5NlD2e48AUB8YLIm12C/y4IBUzqbMjk89ivUS', '김철수', 'STUDENT', now());
-INSERT INTO member (email, password, name, role, created_at) VALUES ('test22@email.com', '{bcrypt}$2a$12$M48gpxGahQ1dXIW0bicv4eallKaCkSKGjTQHzvdbQtv7iLIVSSSEa', '김영희', 'EDUCATOR', now());
-INSERT INTO member (email, password, name, role, created_at) VALUES ('test33@email.com', '{bcrypt}$2a$12$yDElrWj8qwkfrBojYA1r.e2OWUBCtktcZekvhXLlCM2xnwjHdTMDe', '김돌돌', 'STUDENT', now());
+INSERT INTO member (email, password, name, role, created_at) VALUES ('test11@email.com', '{bcrypt}$2a$12$ITtrxyKDNz36rsAMu1NnsebOJWsdMvLZsYmE0tqh0mbC5pNYoSGaK', '김철수', 'STUDENT', now());
+INSERT INTO member (email, password, name, role, created_at) VALUES ('test22@email.com', '{bcrypt}$2a$12$kWIMlnfu.0vBbK4FQbBoHOnVVjZ9cz5qB3W021aW4yB17lyTJkoRu', '김영희', 'EDUCATOR', now());
+INSERT INTO member (email, password, name, role, created_at) VALUES ('test33@email.com', '{bcrypt}$2a$12$YX5PLN9pswbMEoO/XnS4QeZctpYl3Sx9rUPGTF.JrW0WpjCjjWgzK', '김돌돌', 'STUDENT', now());
 
 -- enrollment
 INSERT INTO enrollment(id, status, created_at, member_id) VALUES (1, 'ENROLLED', now(), 1);
