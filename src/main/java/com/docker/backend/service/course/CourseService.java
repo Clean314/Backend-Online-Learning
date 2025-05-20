543b6a7e37d4ffd33ef8581a37666fbb18afaf94
clean314 <pipie314@protonmail.com>
2025-05-20 17:29:01 +0900

    수정

--- src/main/java/com/docker/backend/service/course/CourseService.java
+++ src/main/java/com/docker/backend/service/course/CourseService.java
@@ -1,16 +1,12 @@
 package com.docker.backend.service.course;
 
 import com.docker.backend.dto.CourseCreateRequest;
-import com.docker.backend.dto.CourseDTO;
 import com.docker.backend.entity.Course;
 import com.docker.backend.entity.user.Educator;
-import com.docker.backend.entity.user.Member;
-import com.docker.backend.entity.user.Student;
 import com.docker.backend.repository.MemberRepository;
 import com.docker.backend.repository.course.CourseRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
