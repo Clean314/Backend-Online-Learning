9c07c84631c13cba4d65b2460bdf7bf4dadafbf5
clean314 <pipie314@protonmail.com>
2025-05-31 16:41:34 +0900

    testScore 오류 해결

--- src/main/java/com/docker/backend/entity/TestScore.java
+++ src/main/java/com/docker/backend/entity/TestScore.java
@@ -1,7 +1,12 @@
 package com.docker.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class TestScore {
+    @Id
+    @GeneratedValue
+    private int id;
 }
